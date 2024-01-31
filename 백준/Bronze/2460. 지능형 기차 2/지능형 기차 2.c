3160c5bcad4d88e35f01c92a32151d37d6b86f1c
DeokHwan Kim <30774991+tiger1710@users.noreply.github.com>
2024-01-31 15:43:51 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int people = 0,in,out,i,best=0;
+
+	for (i = 0; i < 10; i++) {
+		scanf("%d %d", &out, &in);
+		people -= out;
+		people += in;
+		best = best < people ? people : best;
+	}
+	printf("%d\n", best);
+
+	return 0;
+}
