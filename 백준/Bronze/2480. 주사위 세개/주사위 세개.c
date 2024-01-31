61cbbdc9e14345fbc4a687d08cc2e3973f15bfbd
DeokHwan Kim <30774991+tiger1710@users.noreply.github.com>
2024-01-31 15:45:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int best(int a, int b, int c) {
+	int A;
+	A = a > b ? a : b;
+	A = c > A ? c : A;
+	return A;
+}
+
+int main(void)
+{
+	int bestscore=0,i;
+
+	int a;
+	int cube[3];
+	scanf("%d %d %d", &cube[0], &cube[1], &cube[2]);
+	if (cube[0] == cube[1] && cube[1] == cube[2]) {
+		bestscore = bestscore < cube[0] * 1000 + 10000 ? cube[0] * 1000 + 10000 : bestscore;
+	}
+	else if (cube[0] != cube[1] && cube[1] != cube[2] && cube[0]!=cube[2]) {
+		a = best(cube[0], cube[1], cube[2]);
+		bestscore = bestscore < a * 100 ? a * 100 : bestscore;
+	}
+	else {
+		if (cube[0] == cube[1]) {
+			a = cube[0];
+			bestscore = bestscore < a * 100 + 1000 ? a * 100 + 1000 : bestscore;
+		}
+		else if (cube[1] == cube[2]) {
+			a = cube[1];
+			bestscore = bestscore < a * 100 + 1000 ? a * 100 + 1000 : bestscore;
+		}
+		else {
+			a = cube[0];
+			bestscore = bestscore < a * 100 + 1000 ? a * 100 + 1000 : bestscore;
+		}
+	}
+	printf("%d\n", bestscore);
+
+
+	return 0;
+}
