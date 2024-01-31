ed53373bdd11a619945a29147672ec724e907f4a
DeokHwan Kim <30774991+tiger1710@users.noreply.github.com>
2024-01-31 15:43:02 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int k = n; k > 0; k--) {
+		for (int i = k; i > 0; i--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
