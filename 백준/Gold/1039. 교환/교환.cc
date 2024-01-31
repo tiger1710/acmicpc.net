f8c8e290a6cf2cb4f5d1e3a67ab60af9fc154653
DeokHwan Kim <30774991+tiger1710@users.noreply.github.com>
2024-01-31 15:05:18 +0900

    [Gold II] Title: 교환, Time: 4 ms, Memory: 2428 KB -BaekjoonHub

---
+++ 백준/Gold/1039. 교환/교환.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+#define ALL(x) (x).begin(), (x).end()
+#define REP(i, from, to) for (int i = (from); i < (to); i++)
+
+string N;
+int K;
+
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> N >> K;
+
+    queue<string> q;
+    q.push(N);
+    REP (i, 0, K) {
+        set<string> s;
+        const size_t qSize = q.size();
+        REP (size, 0, qSize) {
+            const string curr = q.front(); q.pop();
+            if (s.find(curr) not_eq s.end()) {
+                continue;
+            }
+            s.insert(curr);
+
+            REP (u, 0, curr.size()) {
+                REP (v, u + 1, curr.size()) {
+                    string next(curr); swap(next[u], next[v]);
+                    if (next.front() not_eq '0') {
+                        q.push(next);
+                    }
+                }
+            }
+        }
+    }
+
+    string ans("0");
+    while (not q.empty()) {
+        ans = max(ans, q.front());
+        q.pop();
+    }
+
+    if (ans not_eq "0") {
+        cout << ans << endl;
+    } else {
+        cout << -1 << endl;
+    }
+    
+
+    return 0;
+}
